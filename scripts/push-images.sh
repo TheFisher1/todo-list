bf261bc455c448235781573eafcbd0330e4d15e2
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2025-01-01 23:41:30 +0200

    update ci file

--- scripts/push-images.sh
+++ scripts/push-images.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 #$1 - repo, $2 - github commit
-for SERVICE in $(docker-compose ps -q | xargs docker inspect --format '{{.Config.Image}}'); do
-      docker tag $SERVICE $SERVICE
-      docker tag $SERVICE $SERVICE
-
-      echo $SERVICE
-
+for SERVICE in (docker images --format '{{.Repository}}:{{.Tag}}' | grep 'todo-list'); do
       docker push $SERVICE
       docker push $SERVICE
 done
