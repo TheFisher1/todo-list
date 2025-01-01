e3b41df76c5b794b2cfc5312d8c79197e54256b0
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2025-01-01 22:39:25 +0200

    update ci

--- scripts/push-images.sh
+++ scripts/push-images.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #$2 - github commit
-for SERVICE in $(docker-compose config --services); do
-      $VAR = docker-compose ps -q | xargs docker inspect --format '{{.Config.Image}}'
-      docker push $VAR:latest
-      docker push $VAR:$2
+for SERVICE in $(docker-compose ps -q | xargs docker inspect --format '{{.Config.Image}}'); do
+
+      docker push $SERVICE:latest
+      docker push $SERVICE:$2
 done
