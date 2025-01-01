cfb6b8c97b061afbea54d6a7f91d6e339743e4ec
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2025-01-01 22:28:32 +0200

    change ci

--- scripts/push-images.sh
+++ scripts/push-images.sh
@@ -2,6 +2,6 @@
 
 #$1 - dockerhub username, $2 - github commit
 for SERVICE in $(docker-compose config --services); do
-      docker push $1/todo-list-$SERVICE:latest
-      docker push $1/todo-list-$SERVICE:$2
+      docker push $1/$SERVICE:latest
+      docker push $1/$SERVICE:$2
 done
