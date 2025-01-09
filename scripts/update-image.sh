0d8115e55bad5913873902438bb532d5065e5783
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2025-01-09 22:32:08 +0200

    update pipeline

--- scripts/update-image.sh
+++ scripts/update-image.sh
@@ -1,4 +1,4 @@
-kubectl set image deployment/$1 $1=${env.DOCKER_USERNAME}/todo-list:$1-${env.GITHUB_SHA}
+kubectl set image deployment/$1 $1=${DOCKER_USERNAME}/todo-list:$1-${GITHUB_SHA}
 
 kubectl get deployment $1 -o yaml > deployment.yaml
 
