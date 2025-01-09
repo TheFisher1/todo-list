d53a83d69fc0ef4cffebd66a90361b710e524daf
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2025-01-09 16:10:23 +0200

    refactor code

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,4 +1,4 @@
-cd ../task-service && npx run eslint . --ext .js && cd ../
-cd ../user-service && npx run eslint . --ext .js && cd ../
-cd ../frontend-service && npx run eslint . --ext .js --ext .jsx && cd ../
-cd ../gateway && npx run eslint . --ext .js
+cd task-service && npx run eslint . --ext .js && cd ../
+cd user-service && npx run eslint . --ext .js && cd ../
+cd frontend-service && npx run eslint . --ext .js --ext .jsx && cd ../
+cd gateway && npx run eslint . --ext .js
