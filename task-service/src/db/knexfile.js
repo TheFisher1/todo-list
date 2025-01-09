7b097f09ef7aa94bc76883039d3188912551242e
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2025-01-09 16:47:01 +0200

    fix tests

--- task-service/src/db/knexfile.js
+++ task-service/src/db/knexfile.js
@@ -1,15 +1,30 @@
-export const development = {
-  client: 'postgresql',
-  connection: process.env.DATABASE_URL,
-  pool: {
-    min: 2,
-    max: 10
+const development = {
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'todo_list',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_NAME || 'todo_list'
   },
   migrations: {
-    tableName: 'knex_migrations_tasks',
     directory: './migrations'
-  },
-  seeds: {
-    directory: './seeds'
   }
+};
+
+const test = {
+  client: 'pg',
+  connection: {
+    host: process.env.TEST_DB_HOST || 'localhost',
+    user: process.env.TEST_DB_USER || 'test_user',
+    password: process.env.TEST_DB_PASSWORD || 'test_password',
+    database: process.env.TEST_DB_NAME || 'test_todo_list'
+  },
+  migrations: {
+    directory: './migrations'
+  }
+};
+
+module.exports = {
+  development,
+  test
 }; 
