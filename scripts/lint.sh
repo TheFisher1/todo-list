293e45fa5484a5bde3f72cccf952451a46a5c97f
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2025-01-09 16:00:53 +0200

    add Linter

---
+++ scripts/lint.sh
@@ -0,0 +1,4 @@
+cd task-service && eslint . --ext .js,.jsx && cd ../
+cd user-service && eslint . --ext .js,.jsx && cd ../
+cd frontend-service && eslint . --ext .js,.jsx && cd ../
+cd gateway && eslint . --ext .js,.jsx
