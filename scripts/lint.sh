4f801e738c2920c222966b2fdea98ceecdc7c082
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2025-01-09 16:04:00 +0200

    refactor code

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,4 +1,4 @@
-cd task-service && eslint . --ext .js,.jsx && cd ../
-cd user-service && eslint . --ext .js,.jsx && cd ../
-cd frontend-service && eslint . --ext .js,.jsx && cd ../
-cd gateway && eslint . --ext .js,.jsx
+cd task-service && eslint . --ext .js && cd ../
+cd user-service && eslint . --ext .js && cd ../
+cd frontend-service && eslint . --ext .js --ext .jsx && cd ../
+cd gateway && eslint . --ext .js
