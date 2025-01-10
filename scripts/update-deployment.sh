4a39e61bf672ac61726f074979d5c7235be5ce6f
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2025-01-10 10:57:00 +0200

    update deployment saves

--- scripts/update-deployment.sh
+++ scripts/update-deployment.sh
@@ -1,8 +1,8 @@
-kubectl patch deployment frontend --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": ${DOCKER_USERNAME}/todo-list:frontend-${ GITHUB_SHA }}]'
+kubectl patch deployment frontend --type='json' -p="[{\"op\": \"replace\", \"path\": \"/spec/template/spec/containers/0/image\", \"value\": \"${DOCKER_USERNAME}/todo-list:frontend-${GITHUB_SHA}\"}]"
 
-kubectl patch deployment user-service --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": ${DOCKER_USERNAME}/todo-list:user-service-${GITHUB_SHA }}]'
+kubectl patch deployment user-service --type='json' -p="[ {\"op\": \"replace\", \"path\": \"/spec/template/spec/containers/0/image\", \"value\": \"${DOCKER_USERNAME}/todo-list:user-service-${GITHUB_SHA}\"} ]"
 
-kubectl patch deployment task-service --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": ${DOCKER_USERNAME}/todo-list:task-service-${GITHUB_SHA }}]'
+kubectl patch deployment task-service --type='json' -p="[ {\"op\": \"replace\", \"path\": \"/spec/template/spec/containers/0/image\", \"value\": \"${DOCKER_USERNAME}/todo-list:task-service-${GITHUB_SHA}\"} ]"
 
 if kubectl rollout status deployment/frontend; then
      kubectl get deployment frontend -o yaml > K8s/frontend/deployment.yaml
