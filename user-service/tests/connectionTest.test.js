faadaefb4b5b8488429f3f01a5719f4516585422
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2025-01-10 11:20:34 +0200

    update project

---
+++ user-service/tests/connectionTest.test.js
@@ -0,0 +1,17 @@
+const userService = require('./userService');
+
+describe('User Service Tests', () => {
+   test('should connect to the database', async () => {
+    await User.query().insert({
+        name: 'test',
+        email: 'test@test.com',
+        password: 'test'
+    });
+
+    const users = await User.query().where('email', 'test@test.com');
+    expect(users).toHaveLength(1);
+    expect(users[0].name).toBe('test');
+    expect(users[0].email).toBe('test@test.com');
+    expect(users[0].password).toBe('test');
+   });
+});
