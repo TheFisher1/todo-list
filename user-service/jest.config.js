c981e9fe1d504aad4239b67a74b84254067735fd
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2025-01-10 11:44:42 +0200

    update project

---
+++ user-service/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    testEnvironment: 'node',
+    testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+    collectCoverage: false,
+    coverageProvider: 'v8',
+};
