5da11557ee0d592bd44a4deb448b666a3b4af42b
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2025-01-10 09:58:42 +0200

    fix linter

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,27 +1,31 @@
-const globals = require("globals");
-const pluginJs = require("@eslint/js");
-const pluginReact = require("eslint-plugin-react");
-
-
-/** @type {import('eslint').Linter.Config[]} */
-module.exports = [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
-    rules: {
-      'react/prop-types': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react/no-unescaped-entities': 'off',
-      'react/unescaped-entities': 'off',
-    },
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
   },
-  {
-    overrides: [
-      {
-        files: ["src/**/*.jsx", "src/**/*.js"],
-      },
-    ],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
   },
-];
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/no-unescaped-entities': 'off'
+  },
+  ignorePatterns: ['dist/', 'build/', 'node_modules/', '*.config.js']
+};
