5b50c2c2033434bd9be64cef7689ea96491a1023
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2025-01-10 08:25:49 +0200

    fix linter

--- user-service/eslint.config.js
+++ user-service/eslint.config.js
@@ -1,11 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    languageOptions: { globals: globals.node },
-  },
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   {
     files: ["**/*.js"],
