507359077eb9fbfc8f92609b0d99e65e09fa79d6
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2025-01-10 13:47:01 +0200

    update scripts

--- scripts/update-deployment.sh
+++ scripts/update-deployment.sh
@@ -5,14 +5,14 @@ kubectl patch deployment user-service --type='json' -p="[ {\"op\": \"replace\", 
 
 kubectl patch deployment task-service --type='json' -p="[ {\"op\": \"replace\", \"path\": \"/spec/template/spec/containers/0/image\", \"value\": \"${DOCKER_USERNAME}/todo-list:task-service-${GITHUB_SHA}\"} ]"
 
-if kubectl rollout status deployment/frontend; then
+if kubectl rollout --no-wait status deployment/frontend; then
      kubectl get deployment frontend -o yaml > K8s/frontend/deployment.yaml
 fi
 
-if kubectl rollout status deployment/user-service; then
+if kubectl rollout --no-wait status deployment/user-service; then
      kubectl get deployment user-service -o yaml > K8s/users/deployment.yaml
 fi
 
-if kubectl rollout status deployment/task-service; then
+if kubectl rollout --no-wait status deployment/task-service; then
      kubectl get deployment task-service -o yaml > K8s/tasks/deployment.yaml
 fi
